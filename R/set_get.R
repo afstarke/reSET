4469903653f5394e0341aa017737f7bc4aa39d86
Adam Starke <astarke@tnc.org>
2018-11-06 08:55:03 -0500

    initial

---
+++ R/set_get.R
@@ -0,0 +1,17 @@
+#' SET_Connect
+#'
+
+
+
+#' Get SET-MH data from Access DB
+#'
+#' This function connects and returns information relating to the SET-MH database
+#' @param dbPath file url for backend data base.
+#' @keywords SET-MH,
+#' @export
+#' @examples
+#' set_get('data/DB.gdb')
+
+set_get_DB <- function(dbPath){
+
+}
